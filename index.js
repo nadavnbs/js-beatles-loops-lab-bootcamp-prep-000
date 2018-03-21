d489deb94aaedffaa92c62a464c4d09c741903b6
nadav ben shimon <nadavnbs@gmail.com>
2018-03-21 14:36:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musiciansArray,instrumentsArray){
   var whoPlaysWhat = []
   for(var i =0;i < musiciansArray.length;i++){
-    
+    whoPlaysWhat.push(musiciansArray[i] + " plays guitar")
   }
+  return whoPlaysWhat
 }
